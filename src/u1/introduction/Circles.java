6aedf8b66022c7e8f347f05681125e4e252c1da3
PedroAyon <pedroayonb@gmail.com>
2022-08-17 19:01:18 -0500

    Circles

--- src/u1/introduction/Circles.java
+++ src/u1/introduction/Circles.java
@@ -4,25 +4,25 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Circles extends JPanel {
-    int smallestCircleSize;
-    int numberOfCircles;
+    int biggestCircleSize;
+    int sizeDecrement;
 
-    public Circles(int smallestCircleSize, int numberOfCircles) {
-        this.smallestCircleSize = smallestCircleSize;
-        this.numberOfCircles = numberOfCircles;
+    public Circles(int biggestCircleSize, int sizeDecrement) {
+        this.biggestCircleSize = biggestCircleSize;
+        this.sizeDecrement = sizeDecrement;
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        setBackground(Color.BLACK);
+        setBackground(Color.LIGHT_GRAY);
         int xMiddle = getWidth() / 2;
         int yMiddle = getHeight() / 2;
         g.setColor(Color.WHITE);
-        g.drawLine(xMiddle, 0, xMiddle, getHeight());
-        g.drawLine(0, yMiddle, getWidth(), yMiddle);
-        for (int size = smallestCircleSize, i = 0; i < numberOfCircles; size += 50, i++) {
-            g.drawOval(xMiddle - size / 2, yMiddle - size / 2, size, size);
+        for (int size = biggestCircleSize, i = 0; i < biggestCircleSize / sizeDecrement; size -= sizeDecrement, i++) {
+            if (i % 2 == 0) g.setColor(Color.BLACK);
+            else g.setColor(Color.WHITE);
+            g.fillOval(xMiddle - size / 2, yMiddle - size / 2, size, size);
         }
     }
 }
--- src/u1/introduction/Main.java
+++ src/u1/introduction/Main.java
@@ -4,7 +4,10 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        JPanel panel = new Lines(10);
+        // JPanel panel = new Lines(20);
+        JPanel panel = new Circles(900, 20);
+        // JPanel panel = new Drawing(20);
+
         JFrame frame = new JFrame();
         frame.setSize(600, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
